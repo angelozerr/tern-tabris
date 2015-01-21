a62036522bcd25d2e64dd6d590ce596de9374db3
Christian Petrov <cpetrov@eclipsesource.com>
2015-01-21 14:02:06 +0100

    Add CM templates for tabris.js animation props
    Add convenience templates for the animation properties object.

--- codemirror/addon/hint/javascript/tabris-templates.js
+++ codemirror/addon/hint/javascript/tabris-templates.js
@@ -10,6 +10,14 @@
       "name": "properties",
       "description": "With Text, Font and LayoutData containing `left`, `top` and `right` arguments.",
       "template": "{text: ${text}, font: \"14px\", layoutData: {left: 0, top: 0, right: 0}"
+    }, {
+      "name": "animationProperties",
+      "description": "With the `opacity` property set for a fade out effect.",
+      "template": "{opacity: 0}"
+    }, {
+      "name": "animationProperties",
+      "description": "With the `translation` property set for a horizontal translation animation.",
+      "template": "{transform: {translationX: 20}}"
     }]
   };
   CodeMirror.templatesHint.addTemplates(templates);
