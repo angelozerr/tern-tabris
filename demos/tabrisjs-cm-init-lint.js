51d2303a869695eb5c5bd6076c82532e392b921e
angelozerr <angelo.zerr@gmail.com>
2015-02-09 13:56:18 +0100

    Add demo with tabris lint.

---
+++ demos/tabrisjs-cm-init-lint.js
@@ -0,0 +1,48 @@
+$(function() {
+
+  window.cmEditor = window.cmEditor || {};
+  cmEditor.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    mode: 'application/javascript',
+    theme: "eclipse",
+    styleActiveLine: true,
+    lineNumbers: true,
+    lineWrapping: true,
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    extraKeys: {
+      "'.'": passAndHint,
+      "Ctrl-Space": "contextAutocomplete",
+      "Ctrl-I": function(cm) {
+        CodeMirror.tern.showType(cm);
+      },
+      "Alt-.": function(cm) {
+        CodeMirror.tern.jumpToDef(cm);
+      },
+      "Alt-,": function(cm) {
+        CodeMirror.tern.jumpBack(cm);
+      },
+      "Ctrl-Q": function(cm) {
+        CodeMirror.tern.rename(cm);
+      }
+    },
+    gutters: ["CodeMirror-lint-markers", "CodeMirror-linenumbers"],
+    textHover: {
+      delay: 300
+    },
+    ternWith: {
+      plugins: {
+    	"lint": {},  
+        "tabris": {}
+      }
+    },
+    lint: {getAnnotations: CodeMirror.ternLint, async : true, server: CodeMirror.tern.getServer}
+  });
+
+  function passAndHint(cm) {
+    setTimeout(function() {
+      CodeMirror.showHint(cm, CodeMirror.ternHint, {async: true});
+    }, 100);
+    return CodeMirror.Pass;
+  }
+
+});
